90f5216be38ad215c0d17996802198545fcf4a23
Natalia Isabel Zamudio Muñoz <natazm@outlook.es>
2024-05-31 11:48:47 -0500

    t05

--- JS/t05.js
+++ JS/t05.js
@@ -1,15 +1,14 @@
 // Escribe un programa que le pida al usuario ingresar una frase. El programa debe imprimir la frase en la consola 10 veces. NOTA: utiliza un ciclo para imprimir la frase las 10 veces.
 
 let frase = prompt("Ingresar una frase: ");
+let n_repetir  = 10           //prompt("Ingresar cuantas veces desea repetir la frase: ");
 
-
-function repetir(frase){
+function repetir(frase, n_repetir){
     
-    for(let i = 1; i<= 10; i++){
+    for(let i = 1; i<= n_repetir; i++){
 
-        console.log (frase)
-
+        console.log (i +". "+ frase)
     }
        
 }
-repetir(frase)
+repetir(frase, n_repetir)
