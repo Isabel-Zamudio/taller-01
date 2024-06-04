bdec5991671c6932b27e19bf5bcf83fa44ed8c98
Natalia Isabel Zamudio Muñoz <73199137+Isabel-Zamudio@users.noreply.github.com>
2024-06-04 15:50:13 -0500

    Update t04.js

--- JS/t04.js
+++ JS/t04.js
@@ -8,8 +8,10 @@
 let numero = prompt("Digite numero: ");
 
 function mutiplo(numero){
-
-    if (numero % 3 == 0) {
+    if (numero % 3 === 0 && numero % 5 === 0) {
+        console.log("bingbong")
+    }
+   else if (numero % 3 == 0) {
         console.log("bing")
     }
     else if(numero % 5 == 0){
