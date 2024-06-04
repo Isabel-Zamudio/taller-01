cb7e25a3fed6d40c0ec96cf55d8be9434c23fb83
Natalia Isabel Zamudio Muñoz <73199137+Isabel-Zamudio@users.noreply.github.com>
2024-06-04 15:37:24 -0500

    Update t10.js

--- JS/t10.js
+++ JS/t10.js
@@ -11,3 +11,23 @@
 // 24
 // 39
 // NOTA: este ejercicio lo debes hacer con ciclos y sin ayuda de la función filter de JavaScript. Al terminarlo cambia el arreglo original para verificar que funcione con otros números.
+
+const nums = [1, 23, 5, 8, 40, 12, 2, 67, 24, 9, 39];
+
+function agregar(nums) {
+    let newList = []
+    let newList2 = []
+    
+    for (let i = 0; i < nums.length; i++) { 
+        if (nums[i] > 10) {
+            newList.push(nums[i]);
+        } else {
+            newList2.push(nums[i]);
+        }
+    }
+    
+    console.log(newList)
+    console.log(newList2)
+}
+
+agregar(nums)
