ab95fadef169f612c90f7f68ebdbb36fc4f8533c
Natalia Isabel Zamudio Muñoz <73199137+Isabel-Zamudio@users.noreply.github.com>
2024-06-04 15:46:47 -0500

    Update t11.js

--- JS/t11.js
+++ JS/t11.js
@@ -6,3 +6,19 @@
 
 // 11
 // NOTA: este ejercicio lo debes hacer con ciclos y sin ayuda de la función filter de JavaScript. Al terminarlo, cambia el arreglo original para verificar que funcione con otras combinaciones de ceros y unos.
+
+const nums = [ 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0,1,0,0,0,1,1,1,0,0,1,0,1,0,1,1,0,1,0,0,1, 0, 0, 1, 0, 1, 0, 1, 1 ];
+
+function agregar(nums) {
+    let newList = []
+
+    for (let i = 0; i < nums.length; i++) { 
+        if (nums[i] == 1) {
+            newList.push(nums[i]);
+        }
+    
+    }
+    console.log(newList.length)
+}
+
+agregar(nums)
